826c3d15937e757c7eeb48c28dc53cf9f0d5a22b
littbarskiadeh <lit.adeh@gmail.com>
2021-12-24 01:11:00 -0500

    initial commit, added test

---
+++ contracts/starNotary.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract StarNotary {
+    
+    string public starName;
+    address public starOwner;
+    
+    event starClaimed(address owner);
+
+    constructor() {
+        starName = "Super Star";
+    }
+
+    function claimStar() public {
+        starOwner = msg.sender;
+        emit starClaimed(msg.sender);
+    }
+    
+}
